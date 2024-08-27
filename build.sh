2c905d96ec0c73e9afaeddfbf3b820999848cc71
Irefe <nlp_irefe@163.com>
2024-08-28 00:03:21 +0800

    搭建工程

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+build_path="./build"
+output_path="./output"
+
+if [ ! -d "$build_path" ]; then
+    mkdir $build_path
+else
+    rm -rf $build_path/*
+fi
+if [ ! -d "$output_path" ]; then
+    mkdir $output_path
+else
+    rm -rf $output_path/*
+fi
+
+cd ${build_path}
+cmake ..
+make

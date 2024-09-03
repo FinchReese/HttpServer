0364b6f6ff53deff7c92f393ca284c831365a838
Irefe <nlp_irefe@163.com>
2024-09-04 00:03:34 +0800

    处理http请求

---
+++ inc/http_processor.h
@@ -0,0 +1,84 @@
+#ifndef HTTP_PROCESSOR_H
+#define HTTP_PROCESSOR_H
+
+#include <sys/uio.h>
+
+const unsigned int MAX_READ_BUFF_LEN = 2048;
+const unsigned int MAX_WRITE_BUFF_LEN = 1024;
+
+enum GetALineState : unsigned char {
+    GET_A_LINE_OK = 0,
+    GET_A_LINE_CONTINUE = 1,
+    GET_A_LINE_ERROR = 2,
+};
+
+enum HttpProcessState : unsigned int {
+    HTTP_PROCESS_STATE_PARSE_REQUEST_LINE = 0,
+    HTTP_PROCESS_STATE_PARSE_HEAD_FIELD = 1,
+    HTTP_PROCESS_STATE_PARSE_MESSAGE_BODY = 2,
+};
+
+enum ParseRequestReturnCode: unsigned int {
+    PARSE_REQUEST_RETURN_CODE_FINISH = 0, // 解析请求消息完成
+    PARSE_REQUEST_RETURN_CODE_ERROR = 1, // 解析请求消息出错
+    PARSE_REQUEST_RETURN_CODE_CONTINUE = 2, // 需要继续解析
+};
+
+enum ResponseStatusCode : unsigned int {
+    RESPONSE_STATUS_CODE_OK = 200, // 请求成功
+    RESPONSE_STATUS_CODE_BAD_REQUEST = 400, // 通用客户请求错误
+    RESPONSE_STATUS_CODE_FORBIDDEN = 403, // 访问被服务器禁止
+    RESPONSE_STATUS_CODE_NOT_FOUND = 404, // 资源没找到
+    RESPONSE_STATUS_CODE_INTERNAL_SERVER_ERROR = 500, // 通用服务器错误
+};
+
+enum SendResponseReturnCode {
+    SEND_RESPONSE_RETURN_CODE_FINISH = 0; // 发送回复消息完成
+    SEND_RESPONSE_RETURN_CODE_ERROR = 1; // 发送回复消息出错
+    SEND_RESPONSE_RETURN_CODE_AGAIN = 2; // 再试一次
+    SEND_RESPONSE_RETURN_CODE_NEXT = 3; // 进入下一次处理消息流程
+};
+
+enum VectorIndex {
+    STATUS_LINE_AND_HEAD_FIELD_VECTOR_INDEX = 0; // 状态行和头部信息对应向量下标
+    CONTENT_VECTOR_INDEX = 1; // 消息体对应向量下标
+    VECTOR_COUNT,
+};
+
+class HttpProcessor {
+public:
+    HttpProcessor(const int socketId, const char *sourceDir);
+    ~HttpProcessor();
+    bool Read();
+    SendResponseReturnCode Write();
+    void Init();
+    bool ProcessRequest();
+    GetALineState GetALine();
+    ParseRequestReturnCode ParseRequestLine();
+    bool GetField(char *&field);
+private:
+    GetALineState GetALine();
+private:
+    char *m_sourceDir{ nullptr };
+    char m_request[MAX_READ_BUFF_LEN]{ 0 }; // 记录请求报文
+    int m_socketId{ 0 }; // 对应的套接字id
+    unsigned int m_currentRequestSize{ 0 }; // 记录当前收到的请求报文长度
+    char *m_startPos{ nullptr}; // 解析报文字段的起始位置
+    unsigned int m_currentIndex{ 0 }; // 解析报文是否有换行符的当前位置
+    HttpProcessState m_processState{ HTTP_PROCESS_STATE_PARSE_REQUEST_LINE };
+    char *m_method{ nullptr };
+    char *m_url{ nullptr };
+    char *m_httpVersion{ nullptr };
+    unsigned int m_contentLen{ 0 };
+    bool m_keepAlive{ false };
+    char m_writeBuff[MAX_WRITE_BUFF_LEN]{ 0 }; // 记录请求报文
+    unsigned int m_writeSize{ 0 };
+    char *m_fileAddr{ nullptr };
+    unsigned int m_fileSize{ 0 };
+    struct iovec m_iov[VECTOR_COUNT]{ 0 };
+    int m_cnt{ 0 };
+    unsigned int m_leftRespSize{ 0 }; // 剩余回复字节数
+};
+
+
+#endif
---
+++ src/http_processor.cpp
@@ -0,0 +1,503 @@
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/uio.h>
+#include "http_processor.h"
+
+const char *WHITE_SPACE_CHARS = " \t";
+const char *GET_METHOD_STR = "GET";
+const char *URL_HTTP_PREFIX = "http://";
+const char URL_SPLIT_CHAR = '/';
+const char HEAD_FIELD_SPLIT_CHAR = ':';
+const char END_CHAR = '\0'; // 结束符
+const char *CONTENT_LENGTH_KEY_NAME = "Content-Length";
+const char *CONNECTION_KEY_NAME = "Connection";
+const char *KEEP_ALIVE_VALUE = "keep-alive";
+const char *CLOSE_ALIVE_VALUE = "close";
+const char *HTTP_VERSION = "HTTP/1.1";
+const char *OK_TITLE = "OK";
+const char *BAD_REQUEST_TITLE = "Bad Request";
+const char *BAD_REQUEST_CONTENT = "Your request has bad syntax or is inherently impossible to satisfy.\n";
+const char *FORBIDDEN_TITLE = "Forbidden";
+const char *FORBIDDEN_CONTENT = "You don't have permission to get file from this server.\n";
+const char *NOT_FOUND_TITLE = "Not Found";
+const char *NOT_FOUND_CONTENT = "The request file was not found on this server.\n";
+const char *INTERNAL_SERVER_ERROR_TITLE = "Internal Server Error";
+const char *INTERNAL_SERVER_ERROR_CONTENT = "There was an unusual problem serving the requested file.\n";
+const unsigned int MAX_FILE_NAME_LEN = 200;
+
+void (HttpProcessor::*ParseHeadFieldValueStr)();
+typedef struct {
+    const char *keyName;
+    ParseHeadFieldValueStr parseFunc;
+} KeyNameAndParseFuncMap;
+
+const KeyNameAndParseFuncMap KEY_NAME_AND_PARSE_FUNC_MAP_LIST[] = {
+    { CONTENT_LENGTH_KEY_NAME, HttpProcessor::ParseContentLength },
+    { CONNECTION_KEY_NAME, HttpProcessor::ParseConnection },
+};
+const KeyNameAndParseFuncMap KEY_NAME_AND_PARSE_FUNC_MAP_LIST_SIZE =
+    sizeof(KEY_NAME_AND_PARSE_FUNC_MAP_LIST) / sizeof(KEY_NAME_AND_PARSE_FUNC_MAP_LIST[0]);
+
+typedef struct {
+    ResponseStatusCode statusCode;
+    const char *statusTitle;
+    const char *statusContent;
+} StatusInfo;
+
+const StatusInfo ERROR_STATUS_INFO_LIST[] = {
+    { RESPONSE_STATUS_CODE_BAD_REQUEST, BAD_REQUEST_TITLE, BAD_REQUEST_CONTENT },
+    { RESPONSE_STATUS_CODE_FORBIDDEN, FORBIDDEN_TITLE, FORBIDDEN_CONTENT },
+    { RESPONSE_STATUS_CODE_NOT_FOUND, NOT_FOUND_TITLE, NOT_FOUND_CONTENT },
+    { RESPONSE_STATUS_CODE_INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR_TITLE, INTERNAL_SERVER_ERROR_CONTENT },
+};
+const unsigned int ERROR_STATUS_INFO_LIST_SIZE = sizeof(ERROR_STATUS_INFO_LIST) / sizeof(ERROR_STATUS_INFO_LIST[0]);
+
+HttpProcessor::HttpProcessor(socketId, const char *sourceDir) : m_socketId(socketId), m_sourceDir(sourceDir)
+{}
+
+HttpProcessor::~HttpProcessor()
+{}
+
+bool HttpProcessor::Read()
+{
+    ssize_t readSize = read(m_socketId, m_request + m_currentRequestSize, MAX_READ_BUFF_LEN - m_currentRequestSize);
+    if (readSize <= 0) {
+        printf("ERROR read fail, socket id = %d\n", m_socketId);
+        return false;
+    }
+
+    m_currentRequestSize += readSize;
+    return true;
+}
+
+SendResponseReturnCode HttpProcessor::Write()
+{
+    if (m_leftRespSize == 0) {
+        printf("ERROR No content need to send.\n");
+        return SEND_RESPONSE_RETURN_CODE_ERROR;
+    }
+
+    ssize_t ret;
+    while (true) {
+        ret = writev(m_sockfd, m_iov, m_cnt);
+        // 发送回复消息异常
+        if (ret == -1) {
+            if (errno == EAGAIN) {
+                return SEND_RESPONSE_RETURN_CODE_AGAIN;
+            }
+            munmap(m_fileAddr, m_fileSize);
+            m_fileAddr = nullptr;
+            return SEND_RESPONSE_RETURN_CODE_ERROR;
+        }
+        unsigned int writeSize = static_cast<unsigned int >(ret);
+        m_leftRespSize -= writeSize;
+        // 发送回复消息完成
+        if (m_leftRespSize == 0) {
+            munmap(m_fileAddr, m_fileSize);
+            m_fileAddr = nullptr;
+            if (m_keepAlive) {
+                Init();
+                return SEND_RESPONSE_RETURN_CODE_NEXT;
+            }
+            return SEND_RESPONSE_RETURN_CODE_FINISH;
+        }
+        // 更新向量信息
+        if (writeSize >= m_iov[STATUS_LINE_AND_HEAD_FIELD_VECTOR_INDEX].iov_base) {
+            m_iov[STATUS_LINE_AND_HEAD_FIELD_VECTOR_INDEX].iov_len = 0;
+            m_iov[CONTENT_VECTOR_INDEX].iov_base += writeSize;
+            m_iov[CONTENT_VECTOR_INDEX].iov_len -= writeSize;
+        } else {
+            m_iov[STATUS_LINE_AND_HEAD_FIELD_VECTOR_INDEX].iov_base += writeSize;
+            m_iov[STATUS_LINE_AND_HEAD_FIELD_VECTOR_INDEX].iov_len -= writeSize;                
+        }
+    }
+}
+
+void HttpProcessor::Init()
+{
+    (void)memset_s(m_request, sizeof(m_request), 0, sizeof(m_request));
+    m_currentRequestSize = 0;
+    m_startPos = nullptr;
+    m_currentIndex = 0;
+    m_processState = HTTP_PROCESS_STATE_PARSE_REQUEST_LINE;
+    m_method = nullptr;
+    m_url = nullptr;
+    m_httpVersion = nullptr;
+    m_contentLen = 0;
+    m_keepAlive = false;
+    (void)memset_s(m_writeBuff, sizeof(m_writeBuff), 0, sizeof(m_writeBuff));
+    m_writeSize = 0;
+    m_fileAddr = nullptr;
+    m_fileSize = 0;
+    (void)memset_s(m_iov, sizeof(m_iov), 0, sizeof(m_iov));
+    int m_cnt = 0;
+    m_leftRespSize = 0; // 剩余回复字节数
+}
+
+ParseRequestReturnCode HttpProcessor::ParseRequest()
+{
+    GetALineState getALineState;
+    ParseRequestReturnCode ret;
+    while (true) {
+        getALineState = GetALine();
+        switch (getALineState) {
+            case GET_A_LINE_CONTINUE: {
+                return PARSE_REQUEST_RETURN_CODE_CONTINUE;
+            }
+            case GET_A_LINE_ERROR: {
+                return PARSE_REQUEST_RETURN_CODE_ERROR;
+            }
+            case GET_A_LINE_OK: {
+                ret = ParseSingleLine();
+                if (ret == PARSE_REQUEST_RETURN_CODE_CONTINUE) {
+                    break;
+                } else {
+                    return ret;
+                }
+            }
+            default: {
+                return PARSE_REQUEST_RETURN_CODE_ERROR;
+            }
+        }
+    }
+
+    return PARSE_REQUEST_RETURN_CODE_ERROR;
+}
+
+// 尝试找到"\r\n"，是换行的标记
+GetALineState HttpProcessor::GetALine()
+{
+    if (m_currentIndex >= m_currentRequestSize) {
+        return GET_A_LINE_CONTINUE;
+    }
+
+    while (m_currentIndex < m_currentRequestSize) {
+        switch (m_request[m_currentIndex]) {
+            case '\r': {
+                if (m_currentIndex + 1 < m_currentRequestSize) {
+                    if (m_request[m_currentIndex + 1] == '\n') {
+                        m_request[m_currentIndex++] = END_CHAR ;
+                        m_request[m_currentIndex++] = END_CHAR ;
+                        return GET_A_LINE_OK;
+                    } else {
+                        return GET_A_LINE_ERROR;
+                    }
+                } else {
+                    return GET_A_LINE_CONTINUE;
+                }
+            }
+            case '\n': {
+                return GET_A_LINE_ERROR;
+            }
+            default: {
+                m_currentIndex++;
+                break;
+            }
+        }
+    }
+    return GET_A_LINE_CONTINUE;
+}
+
+ParseRequestReturnCode HttpProcessor::ParseSingleLine()
+{
+    switch (m_processState) {
+        case HTTP_PROCESS_STATE_PARSE_REQUEST_LINE: {
+            return ParseRequestLine();
+        }
+        case HTTP_PROCESS_STATE_PARSE_HEAD_FIELD: {
+            return ParseHeadFields();
+        }
+        case HTTP_PROCESS_STATE_PARSE_MESSAGE_BODY: {
+            return ParseContent();
+        }
+        default: {
+            printf("ERROR Invalid state:%u.\n", m_processState);
+            return PARSE_REQUEST_RETURN_CODE_ERROR;
+        }
+    }
+}
+
+ParseRequestReturnCode HttpProcessor::ParseRequestLine()
+{
+    if (GetField(m_method) == false) {
+        printf("ERROR Get method fail.\n");
+        return PARSE_REQUEST_RETURN_CODE_ERROR;
+    }
+    if (strcasecmp(m_method, GET_METHOD_STR) != 0) {
+        printf("ERROR Support GET method only.\n");
+        return PARSE_REQUEST_RETURN_CODE_ERROR;
+    }
+
+    if (GetField(m_url) == false) {
+        printf("ERROR Get url fail.\n");
+        return PARSE_REQUEST_RETURN_CODE_ERROR;
+    }
+
+    if (strncasecmp(m_url, URL_HTTP_PREFIX, strlen(URL_HTTP_PREFIX)) == 0) {
+        m_url += strlen(URL_HTTP_PREFIX);
+        m_url = strchr(m_url, URL_SPLIT_CHAR);
+    }
+    if (m_url == nullptr || m_url[0] != URL_SPLIT_CHAR) {
+        printf("ERROR Invalid url.\n");
+        return PARSE_REQUEST_RETURN_CODE_ERROR;
+    }
+    
+    m_httpVersion = m_startPos;
+    m_startPos += (strlen(m_startPos) + 1);
+    if (strcasecmp(m_httpVersion, "HTTP/1.0") != 0) {
+        printf("ERROR Support HTTP/1.0 only.\n");
+        return PARSE_REQUEST_RETURN_CODE_ERROR;
+    }
+
+    printf("EVENT Req info: %s %s %s\n", m_method, m_url, m_httpVersion);
+    m_processState = HTTP_PROCESS_STATE_PARSE_HEAD_FIELD;
+    return PARSE_REQUEST_RETURN_CODE_CONTINUE;
+}
+
+bool HttpProcessor::GetField(char *&field)
+{
+    char *ret = strpbrk(m_startPos, WHITE_SPACE_CHARS);
+    if (ret == nullptr) {
+        printf("ERROR Can't find white-space char: %s\n", m_startPos);
+        return false;
+    }
+    *ret = END_CHAR ;
+    field = m_startPos;
+    ret++;
+    m_startPos = ret + strspn(ret, whiteSpaceChars);
+    return true;
+}
+
+ParseRequestReturnCode HttpProcessor::ParseHeadFields()
+{
+    if (m_startPos == END_CHAR) {
+        if (m_contentLen != 0) {
+            m_processState = HTTP_PROCESS_STATE_PARSE_MESSAGE_BODY;
+            return PARSE_REQUEST_RETURN_CODE_CONTINUE;
+        }
+        return PARSE_REQUEST_RETURN_CODE_FINISH;
+    }
+
+    for (unsigned int i = 0; i < KEY_NAME_AND_PARSE_FUNC_MAP_LIST_SIZE; ++i) {
+        KeyNameAndParseFuncMap map = KEY_NAME_AND_PARSE_FUNC_MAP_LIST[i];
+        if (strncasecmp(m_startPos, map.keyName, strlen(map.keyName)) != 0) {
+            continue;
+        }
+        m_startPos += strlen(map.keyName);
+        if (m_startPos != HEAD_FIELD_SPLIT_CHAR) {
+            printf("ERROR invalid head field:%s.\n", m_startPos);
+            return PARSE_REQUEST_RETURN_CODE_ERROR;
+        }
+        m_startPos += 1; // 跳过':'
+        m_startPos += strspn(m_startPos, "\t ");
+        map.parseFunc();
+        m_startPos += (strlen(m_startPos) + 2); // 2表示跳过\r\n
+        return PARSE_REQUEST_RETURN_CODE_CONTINUE;
+    }
+    printf("DEBUG Not match req head:%s.\n", m_startPos);
+    m_startPos += (strlen(m_startPos) + 2); // 2表示跳过\r\n
+    return PARSE_REQUEST_RETURN_CODE_CONTINUE;
+}
+
+void HttpProcessor::ParseContentLength()
+{
+    m_contentLen = atol(m_startPos);
+}
+
+void HttpProcessor::ParseConnection()
+{
+    if (strcasecmp(m_startPos, KEEP_ALIVE_VALUE) == 0) {
+        m_keepAlive = true;
+    }
+}
+
+ParseRequestReturnCode HttpProcessor::ParseContent()
+{
+    unsigned int parseSize = m_startPos - m_request; // 消息体前面信息所占字节数
+    if (m_contentLen > MAX_READ_BUFF_LEN - parseSize) {
+        printf("ERROR invalid Content-Length, m_contentLen = %u, parseSize = %u\n",
+            m_contentLen, parseSize);
+        return PARSE_REQUEST_RETURN_CODE_ERROR;
+    }
+
+    if (m_currentRequestSize - parseSize >= m_contentLen) {
+        m_startPos[m_contentLen] = END_CHAR ;
+        return PARSE_REQUEST_RETURN_CODE_FINISH;
+    }
+
+    return PARSE_REQUEST_RETURN_CODE_CONTINUE;
+}
+
+bool HttpProcessor::Response(const ParseRequestReturnCode returnCode)
+{
+    switch (returnCode) {
+        case PARSE_REQUEST_RETURN_CODE_FINISH: {
+            ResponseStatusCode statusCode = HandleRequest();
+            return AddResponse(statusCode);
+        }
+        case PARSE_REQUEST_RETURN_CODE_ERROR: {
+            return AddResponse(RESPONSE_STATUS_CODE_BAD_REQUEST);
+        }
+        case ARSE_REQUEST_RETURN_CODE_CONTINUE: {
+            return true;
+        }
+        default: {
+            break;
+        }
+    }
+    return false;
+}
+
+ResponseStatusCode HttpProcessor::HandleRequest()
+{
+    char filePath[MAX_FILE_NAME_LEN] = { 0 };
+    int ret = sprintf_s(filePath, MAX_FILE_NAME_LEN, "%s%s", m_sourceDir, m_url);
+    if (ret == -1) {
+       printf("ERROR Get file path fail, dir:%s, url:%s.\n", m_sourceDir, m_url);
+       return RESPONSE_STATUS_CODE_INTERNAL_SERVER_ERROR; 
+    }
+
+    struct stat fileStat{ 0 };
+    if (stat(filePath, &fileStat) == -1) {
+       printf("ERROR Get file stat fail, path:%s.\n", filePath);
+       return RESPONSE_STATUS_CODE_NOT_FOUND;        
+    }
+
+    if ((fileStat.st_mode & S_IROTH) == 0) {
+        printf("ERROR Can't read %s.\n", filePath);
+        return RESPONSE_STATUS_CODE_FORBIDDEN;
+    }
+
+    if (S_ISDIR(fileStat.st_mode)) {
+        printf("ERROR %s is dir.\n", filePath);
+        return RESPONSE_STATUS_CODE_BAD_REQUEST;
+    }
+
+    int fd = open(filePath, O_RDONLY);
+    if (fd == -1) {
+        printf("ERROR open file fail: %s.\n", filePath);
+        return RESPONSE_STATUS_CODE_INTERNAL_SERVER_ERROR; 
+    }
+    m_fileSize = fileStat.st_size;
+    m_fileAddr = reinterpret_cast<char *>mmap(0, m_fileSize, PORT_READ, MAP_PRIVATE, fd, 0);
+    close(fd);
+    return RESPONSE_STATUS_CODE_OK;
+}
+
+bool HttpProcessor::AddResponse(const ResponseStatusCode statusCode)
+{
+    for (unsigned int i = 0; i < ERROR_STATUS_INFO_LIST_SIZE; ++i) {
+        if (statusCode == RESPONSE_STATUS_CODE_OK) {
+            return AddResponseInNormalCase();
+        }
+        if (statusCode == ERROR_STATUS_INFO_LIST[i].statusCode) {
+            return AddResponseInErrorCase(ERROR_STATUS_INFO_LIST[i]);
+        }
+    }
+
+    printf("ERROR Invalid statusCode: %u.\n", statusCode);
+    return false;
+}
+bool HttpProcessor::AddResponseInNormalCase()
+{
+    if (!AddStatusLine(RESPONSE_STATUS_CODE_OK, OK_TITLE)) {
+        return false;
+    }
+    if (!AddHeadField(strlen(OK_TITLE))) {
+        return false;
+    }
+
+    m_iov[STATUS_LINE_AND_HEAD_FIELD_VECTOR_INDEX].iov_base = m_writeBuff;
+    m_iov[STATUS_LINE_AND_HEAD_FIELD_VECTOR_INDEX].iov_len = m_writeSize;
+    m_iov[CONTENT_VECTOR_INDEX].iov_base = m_fileAddr;
+    m_iov[CONTENT_VECTOR_INDEX].iov_len = m_fileSize;
+    m_cnt = 2;
+    m_leftRespSize = m_writeSize + m_fileSize;
+    return true;
+}
+
+bool HttpProcessor::AddResponseInErrorCase(const StatusInfo statusInfo)
+{
+    if (!AddStatusLine(statusInfo.statusCode, statusInfo.statusTitle)) {
+        return false;
+    }
+    if (!AddHeadField(strlen(statusInfo.statusTitle))) {
+        return false;
+    }
+    if (!AddContent(statusInfo.statusContent)) {
+        return false;
+    }
+
+    m_iov[STATUS_LINE_AND_HEAD_FIELD_VECTOR_INDEX].iov_base = m_writeBuff;
+    m_iov[STATUS_LINE_AND_HEAD_FIELD_VECTOR_INDEX].iov_len = m_writeSize;
+    m_cnt = 1;
+    m_leftRespSize = m_writeSize;
+    return true;
+}
+
+bool HttpProcessor::AddStatusLine(const int status, const char *title)
+{
+    if (m_writeSize >= MAX_WRITE_BUFF_LEN) {
+        printf("ERROR Write buffer is full, m_writeSize = %u\n", m_writeSize);
+        return false;
+    }
+    int ret = sprintf_s(m_writeBuff, MAX_WRITE_BUFF_LEN - m_writeSize, "%s %d %s\r\n",
+        HTTP_VERSION, status, title);
+    if (ret == -1) {
+        printf("ERROR Write buffer fail.\n");
+        return false;
+    }
+    m_writeSize += static_cast<unsigned int>(ret);
+
+    return true;
+}
+
+bool HttpProcessor::AddHeadField(const unsigned int contentLen)
+{
+    if (m_writeSize >= MAX_WRITE_BUFF_LEN) {
+        printf("ERROR Write buffer is full, m_writeSize = %u\n", m_writeSize);
+        return false;
+    }
+    // 添加消息体长度
+    int ret = sprintf_s(m_writeBuff, MAX_WRITE_BUFF_LEN - m_writeSize,
+        "Content-Length: %d\r\n", contentLen);
+    if (ret == -1) {
+        printf("ERROR Write buffer fail.\n");
+        return false;
+    }
+    m_writeSize += static_cast<unsigned int>(ret);
+    // 添加连接方式
+    ret = sprintf_s(m_writeBuff, MAX_WRITE_BUFF_LEN - m_writeSize,
+        "Connection: %s\r\n", m_keepAlive ? KEEP_ALIVE_VALUE : CLOSE_ALIVE_VALUE);
+    if (ret == -1) {
+        printf("ERROR Write buffer fail.\n");
+        return false;
+    }
+    m_writeSize += static_cast<unsigned int>(ret);
+    // 添加空行
+    ret = sprintf_s(m_writeBuff, MAX_WRITE_BUFF_LEN - m_writeSize, "\r\n");
+    if (ret == -1) {
+        printf("ERROR Write buffer fail.\n");
+        return false;
+    }
+    m_writeSize += static_cast<unsigned int>(ret);
+    return true;
+}
+
+bool HttpProcessor::AddContent(const char *content)
+{
+    if (m_writeSize >= MAX_WRITE_BUFF_LEN) {
+        printf("ERROR Write buffer is full, m_writeSize = %u\n", m_writeSize);
+        return false;
+    }
+
+    int ret = sprintf_s(m_writeBuff, MAX_WRITE_BUFF_LEN - m_writeSize, "%s\r\n",
+        content);
+    if (ret == -1) {
+        printf("ERROR Write buffer fail.\n");
+        return false;
+    }
+    m_writeSize += static_cast<unsigned int>(ret);
+    return true;
+}
